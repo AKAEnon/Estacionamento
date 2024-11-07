7d2be03793d20fcc7f210a98a2ff47f741de59fc
AKAEnon <enondemori@gmail.com>
2024-11-07 19:23:34 -0300

    Commit 2

--- src/sos/Vagas.java
+++ src/sos/Vagas.java
@@ -1,59 +1,58 @@
 package sos;
 
 public class Vagas {
-	
- int vaga = 0;
- String tamanho;
- boolean disponivel;
- 
-public Vagas() {
+    int vaga;
+    String tamanho;
+    boolean disponivel;
+    Veiculo veiculoAtual;
 
+    public Vagas(int vaga, String tamanho, boolean disponivel) {
+        this.vaga = vaga;
+        this.tamanho = tamanho;
+        this.disponivel = disponivel;
+        this.veiculoAtual = null;
+    }
+
+    public Veiculo getVeiculoAtual() {
+        return veiculoAtual;
+    }
+
+    public void setVeiculoAtual(Veiculo veiculo) {
+        this.veiculoAtual = veiculo;
+    }
+
+    public int getVaga() {
+        return vaga;
+    }
+
+    public void setVaga(int vaga) {
+        this.vaga = vaga;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    @Override
+    public String toString() {
+        if (disponivel) {
+            return "Vaga número: " + vaga + ", Tamanho: " + tamanho + " (Disponível)";
+        } else {
+            return "Vaga número: " + vaga + ", Tamanho: " + tamanho + ", Ocupada por veículo de placa: " + veiculoAtual.getPlaca();
+        }
+    }
 }
 
-public Vagas(int vaga, String tamanho, boolean disponível) {
-	super();
-	this.vaga = vaga;
-	this.tamanho = tamanho;
-	this.disponivel = disponível;
 }
-
-public int getVaga() {
-	return vaga;
-}
-
-public void setVaga(int vaga) {
-	this.vaga = vaga;
-}
-
-public String getTamanho() {
-	return tamanho;
-}
-
-public void setTamanho(String tamanho) {
-	this.tamanho = tamanho;
-}
-
-public boolean isDisponível() {
-	return disponivel;
-}
-
-public void setDisponível(boolean disponível) {
-	this.disponivel = disponível;
-}
-
-@Override
-public String toString() {
-	if (disponivel) {
-	return "Vaga número: " + vaga + " Tamanho:" + tamanho + " A vaga está disponível";
-
-}
-	else {
-		return "Vaga número: " + vaga + " Tamanho:" + tamanho + " A vaga não está disponível";
-	
-	}
-}
-}
-
-
- 
-
